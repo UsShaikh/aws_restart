be956b2eccdbff003838fe558921d058c2cf107d
Umar farooque <126644735+UsShaikh@users.noreply.github.com>
2023-11-24 11:52:04 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,17 @@
+print("Python has three numeric types: int, float, and complex")
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue1=3.14
+print(myValue1)
+print(type(myValue1))
+print(str(myValue1) + " is of the data type " + str(type(myValue1)))
+myValue2=5j
+print(myValue2)
+print(type(myValue2))
+print(str(myValue2) + " is of the data type " + str(type(myValue2)))
+myValue3=True
+print(myValue3)
+print(type(myValue3))
+print(str(myValue3) + " is of the data type " + str(type(myValue3)))
---
+++ loops
@@ -0,0 +1,3 @@
+myMixedTypeList = [234, 2904548, 45.02, False, "I am animal Lover.", "34"]
+for item in myMixedTypeList:
+    print("{} is of the data type {}".format(item,type(item)))
